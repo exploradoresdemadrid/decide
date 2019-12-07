7b354cc929ce1fe90be283dfe14232a2023a74d0
Manuel Bustillo <mayn13@gmail.com>
2019-12-07 20:09:31 +0100

    Remove old asset files from manifest

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,5 +1,3 @@
 //= link_tree ../images
 //= link_directory ../javascripts .js
 //= link_directory ../stylesheets .css
-//= link swagger_ui_engine/application.css
-//= link swagger_ui_engine/application.js
