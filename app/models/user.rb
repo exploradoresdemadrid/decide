5043ffbfc8d85827b97589cf39c52baf7c798049
Manuel Bustillo <mayn13@gmail.com>
2019-12-17 22:30:47 +0100

    Generate auth token on user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,19 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: JWTBlacklist
+
+  AUTH_TOKEN_LENGTH = 6
+
+  before_create :assign_auth_token
+
+  private
+
+  def assign_auth_token
+    self.auth_token = AUTH_TOKEN_LENGTH.times.map { (0..9).to_a.sample.to_s }.join
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,24 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  describe 'validations' do
+    subject { build :user }
+
+    describe 'title' do
+      it { should validate_presence_of(:email) }
+    end
+  end
+
+  describe 'auth_token' do
+    it 'is generated on creation' do
+      expect(create(:user).auth_token).not_to be_empty
+    end
+
+    it 'contains 6 digits' do
+      expect(create(:user).auth_token).to match(/^\d{6}$/)
+    end
+  end
 end
