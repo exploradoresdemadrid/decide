f5c4c3c5c1f2d17a84f7210a7167c0805dd6e89a
Manuel Bustillo <mayn13@gmail.com>
2019-12-25 20:38:21 +0100

    Options scaffolding

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :voting
+  has_many :options
 
   validates_presence_of :title
 end
---
+++ spec/factories/options_factory.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :option do
+    title { "MyString" }
+    question { "" }
+  end
+end
