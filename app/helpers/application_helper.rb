9ef491b96ed49b71cb38cbb148dc8560581ffa89
Manuel Bustillo <mayn13@gmail.com>
2020-01-03 14:26:38 +0100

    Safe check in case no group supported an option

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
   def string_list(items)
+    return unless items.present?
+
     content_tag(:ul) do
       items.map do |group_name|
         content_tag(:li) { group_name }
