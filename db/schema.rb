6664880f00357e460cfc737491c3f3505cc735c3
Manuel Bustillo <mayn13@gmail.com>
2020-01-03 11:35:16 +0100

    Add private column to votings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_29_102407) do
+ActiveRecord::Schema.define(version: 2020_01_03_103404) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2019_12_29_102407) do
     t.integer "status", default: 0, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "private", default: false, null: false
   end
 
   add_foreign_key "groups", "users"
