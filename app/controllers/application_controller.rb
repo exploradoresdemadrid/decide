20c6f6cf97b06103426f6cfd7e65cb5487276b65
Manuel Bustillo <mayn13@gmail.com>
2020-06-20 17:20:31 +0200

    Remove method that no longer exists

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,6 @@
 class ApplicationController < ActionController::Base
   skip_before_action :verify_authenticity_token
   before_action :authenticate_user!
-  serialization_scope :view_context
 
   add_flash_types :error
 
