b9de1e236aa675e748d445af75e9e749e03463c3
Manuel Bustillo <mayn13@gmail.com>
2020-06-20 13:40:32 +0200

    Remove unnecessary condition in ability.rb

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,10 +4,6 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    cannot [:manage], Question do |question|
-      !question.voting.draft?
-    end
-
     if user.superadmin?
       can :manage, :all
     elsif user.admin?
