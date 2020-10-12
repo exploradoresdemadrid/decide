1e6d555b4dafd4c5b303ee28ba3ceed8e1d8db7a
Manuel Hernandez <manuel.hna@gmail.com>
2020-10-12 18:21:25 +0200

    Fixed: #255 "Ready" status for votings
    Visual part implemented. New state "ready".

--- app/models/voting.rb
+++ app/models/voting.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class Voting < ApplicationRecord
-  enum status: %i[draft open finished]
+  enum status: %i[draft open finished ready]
 
   belongs_to :organization
   has_many :questions, dependent: :destroy
