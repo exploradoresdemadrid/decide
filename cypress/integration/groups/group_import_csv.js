d8e7069d5269fa485a53b52c4a48f8ab27ccd24c
Manuel Bustillo <mayn13@gmail.com>
2020-10-14 00:55:24 +0200

    Add cypress test for error handling

--- cypress/integration/groups/group_import_csv.js
+++ cypress/integration/groups/group_import_csv.js
@@ -1,7 +1,7 @@
 /// <reference types="cypress" />
 
 context('Group CSV import', () => {
-  before(() => {
+  beforeEach(() => {
     cy.clearCookies()
     cy.visit('http://localhost:3000/users/sign_in')
     cy.loginAsAdmin()
@@ -16,4 +16,12 @@ context('Group CSV import', () => {
     cy.get('.alert.alert-info').should('contain', 'Se han actualizado 3 grupos.')
   })
 
+  it('Submit invalid information', () => {
+    cy.contains('Grupos').click()
+    cy.contains('Importar CSV').click()
+    cy.get('textarea').invoke('val', 'name,votes\nGroup 1,1\nGroup 2,-2\nGroup 3,3')
+    cy.contains('Enviar').click()
+
+    cy.get('.alert.alert-danger').should('contain', 'La validación falló: Available votes debe ser mayor que o igual a 1')
+  })
 })
