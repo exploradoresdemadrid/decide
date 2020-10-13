638178604743c211c48f0a20bf514d635a95ef52
Manuel Bustillo <mayn13@gmail.com>
2020-10-14 00:17:30 +0200

    Fix cypress test

--- cypress/integration/groups/group_import_csv.js
+++ cypress/integration/groups/group_import_csv.js
@@ -12,7 +12,6 @@ context('Group CSV import', () => {
     cy.contains('Importar CSV').click()
     cy.get('textarea').invoke('val', 'name,votes\nGroup 1,1\nGroup 2,2\nGroup 3,3')
     cy.contains('Enviar').click()
-    cy.contains('Grupos').click()
 
     cy.get('.alert.alert-info').should('contain', 'Se han actualizado 3 grupos.')
   })
