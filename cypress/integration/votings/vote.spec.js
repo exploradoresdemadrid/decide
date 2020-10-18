135891ddd9f6bcdb1e438d6b325c0744dda96087
Manuel Bustillo <mayn13@gmail.com>
2020-10-18 13:12:54 +0200

    Fix cypress specs

--- cypress/integration/votings/vote.spec.js
+++ cypress/integration/votings/vote.spec.js
@@ -65,13 +65,13 @@ context('Vote submission', () => {
     cy.get('.total-votes-counter').should('contain', '5/5')
     cy.get('body').contains('Emitir votos').click()
 
-    cy.get('.alert.alert-danger').should('contain', 'Tu papeleta ha sido enviada. En cuanto se cierre la urna virtual podrás ver los resultados.')
+    cy.get('.alert.alert-success').should('contain', 'Tu papeleta ha sido enviada. En cuanto se cierre la urna virtual podrás ver los resultados.')
   })
 
   it('stay on the same page on reload', () => {
     cy.contains('Recargar').click()
 
-    cy.get('.alert.alert-danger').should('contain', 'Tu papeleta ha sido enviada. En cuanto se cierre la urna virtual podrás ver los resultados.')
+    cy.get('.alert.alert-success').should('contain', 'Tu papeleta ha sido enviada. En cuanto se cierre la urna virtual podrás ver los resultados.')
   })
 
   it('shows results once voting is finished', () => {
