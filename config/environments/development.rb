b7ef6194f19b9c27c36119c3ad2512fe00f8aa80
Manuel Bustillo <mayn13@gmail.com>
2020-10-18 22:25:18 +0200

    Configure EN as the default language for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,5 +60,6 @@ Rails.application.configure do
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
+  config.i18n.default_locale = :en
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
