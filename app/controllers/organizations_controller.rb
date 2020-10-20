9d26239d5732dd627cef143d2dd298396da50556
Manuel Bustillo <mayn13@gmail.com>
2020-10-20 20:53:26 +0200

    Create admin account together with the organization

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -53,6 +53,6 @@ class OrganizationsController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def organization_params
-      params.require(:organization).permit(:name)
+      params.require(:organization).permit(:name, :admin_email, :admin_password)
     end
 end
