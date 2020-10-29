8a4e83937da87dd8f18659994c7b22f982ed2d37
Manuel Bustillo <mayn13@gmail.com>
2020-10-30 00:11:16 +0100

    Configure default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,4 +102,9 @@ Rails.application.configure do
     authentication:       'plain',
     enable_starttls_auto: true
   }
+
+  config.action_mailer.default_url_options = {
+    host: ENV['DECIDE_HOST'],
+    protocol: 'https'
+  }
 end
