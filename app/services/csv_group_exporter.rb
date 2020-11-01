9897a8b46dcf825643fb053ee5b8b91478f9b610
Manuel Bustillo <mayn13@gmail.com>
2020-11-01 11:37:17 +0100

    Initial implementation of CSV importer

--- app/services/csv_group_exporter.rb
+++ app/services/csv_group_exporter.rb
@@ -8,8 +8,35 @@ class CsvGroupExporter
   end
 
   def export!
+    CSV.generate do |csv|
+      csv << headers
+      @organization.groups.each { |g| csv << build_row(g) }
+    end
   end
 
   private
 
+  def headers
+    [
+      'ID',
+      'Name',
+      'Number (optional)',
+      'Email',
+      *bodies.pluck(:name).map { |b| "Votes in #{b}" }
+    ]
+  end
+
+  def build_row(group)
+    [
+      group.id,
+      group.name,
+      group.number,
+      group.email,
+      *bodies.map { |b| group.votes_in_body(b) }
+    ]
+  end
+
+  def bodies
+    @organization.bodies.order(name: :asc)
+  end
 end
