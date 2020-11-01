2d8b4817a520f4d0b7d27a1970e548cf340547b3
Manuel Bustillo <mayn13@gmail.com>
2020-11-01 11:25:30 +0100

    Create scaffold to download CSV template

---
+++ app/services/csv_group_exporter.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+require 'csv'
+
+class CsvGroupExporter
+  def initialize(organization)
+    @organization = organization
+  end
+
+  def export!
+  end
+
+  private
+
+end
