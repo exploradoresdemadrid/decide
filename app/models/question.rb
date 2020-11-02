54de9f291278baa4d84fd31edb351b6095f1ab49
Manuel Bustillo <mayn13@gmail.com>
2020-11-02 19:50:38 +0100

    Use number of votes in the specific body

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,4 +6,6 @@ class Question < ApplicationRecord
   accepts_nested_attributes_for :options, reject_if: :all_blank, allow_destroy: true
 
   validates_presence_of :title
+
+  delegate :body, to: :voting
 end
