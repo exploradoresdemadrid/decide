76c2e3c9018c3ca0be7c24e45d8c7c89b41b282f
Manuel Bustillo <mayn13@gmail.com>
2020-11-21 23:19:07 +0100

    Order questions by title by default

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ApplicationRecord
   belongs_to :voting
   has_many :options, dependent: :destroy
   has_many :votes, through: :options
+  default_scope { order(title: :asc) }
 
   accepts_nested_attributes_for :options, reject_if: :all_blank, allow_destroy: true
 
