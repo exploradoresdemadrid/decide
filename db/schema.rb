45ba3fa3ee64caaee3657e865a663fe24f4d7f38
Manuel Bustillo <mayn13@gmail.com>
2020-12-20 20:13:34 +0100

    Add member type field to organization model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_13_182415) do
+ActiveRecord::Schema.define(version: 2020_12_20_191203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_11_13_182415) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "member_type", default: 0, null: false
   end
 
   create_table "questions", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
